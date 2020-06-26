a39f9be52929ce182ee918107be43c81c1f9eefa
RuChiang <jgchengru@gmail.com>
2020-06-26 21:45:45 +0800

    updated README

--- README.md
+++ README.md
@@ -3,13 +3,23 @@
 pip install -r requirements.txt
 ```
 
-# 執行
+# 執行抓取股價
 ```
-python3 treasury_stock_price_fetcher.py src_data/t35sc09_20200618_23493912.csv <股票代碼>
+python3 treasury_stock_price_fetcher.py src_data/treasury_stock_records.csv <股票代碼>
 
 for example
-python3 treasury_stock_price_fetcher.py src_data/t35sc09_20200618_23493912.csv 2303
+python3 treasury_stock_price_fetcher.py src_data/treasury_stock_records.csv 2303
 
 ```
+
+# 執行指定格式分析
+```
+python3 easury_stock_analyzer.py src_data/treasury_stock_records.csv <股票代碼>
+
+for example
+python3 teasury_stock_analyzer.py src_data/treasury_stock_records.csv 2303
+
+```
+
 # 輸出資料
 在 `/data` 資料夾
--- treasury_stock_price_fetcher.py
+++ treasury_stock_price_fetcher.py
@@ -50,13 +50,13 @@ class Recorder(object):
 
 
 def get_stock_info(idx, date, data_type):
-    sleep(0.5)
+    sleep(5)
     return json.loads(req.get(f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date={date}&stockNo={idx}&_=1592225418511",
                               headers={'Accept-Language': 'zh-TW'}).text)[data_type]
 
 
 def get_index_info(date, data_type):
-    sleep(0.5)
+    sleep(5)
     return json.loads(req.get(f"https://www.twse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date={date}&_=1592225418511",
                               headers={'Accept-Language': 'zh-TW'}).text)[data_type]
 
@@ -66,147 +66,147 @@ def main():
     # get src file
     src_data = sys.argv[1]
     # get list of stock indices
-    target = sys.argv[2]
-    file_reader = FileReader(src_data, target)
-    date_ranges = file_reader.get_date_range()
+    targets = sys.argv[2:]
 
-    logger.debug(f"date_ranges: {date_ranges}")
+    for target in targets:
+        file_reader = FileReader(src_data, target)
+        date_ranges = file_reader.get_date_range()
+        logger.debug(f"date_ranges: {date_ranges}")
 
-    for date_range in date_ranges:
+        for date_range in date_ranges:
 
-        logger.info(f"getting price range of {date_range}")
-        start, end = [''.join(date.split('/')) for date in date_range]
-        #  if start and end are in the same month
+            logger.info(f"getting price range of {date_range}")
+            start, end = [''.join(date.split('/')) for date in date_range]
+            #  if start and end are in the same month
 
-        start_yr = start[0:4]
-        start_month = start[4:6]
-        start_date = start[6:8]
+            start_yr = start[0:4]
+            start_month = start[4:6]
+            start_date = start[6:8]
 
-        end_yr = end[0:4]
-        end_month = end[4:6]
-        end_date = end[6:8]
+            end_yr = end[0:4]
+            end_month = end[4:6]
+            end_date = end[6:8]
 
-        logger.debug(f"start_yr {start_yr}")
-        logger.debug(f"start_month {start_month}")
-        logger.debug(f"start_date {start_date}")
+            logger.debug(f"start_yr {start_yr}")
+            logger.debug(f"start_month {start_month}")
+            logger.debug(f"start_date {start_date}")
 
-        logger.debug(f"end_yr {end_yr}")
-        logger.debug(f"end_month {end_month}")
-        logger.debug(f"end_date {end_date}")
+            logger.debug(f"end_yr {end_yr}")
+            logger.debug(f"end_month {end_month}")
+            logger.debug(f"end_date {end_date}")
 
-        [curr_year, curr_month, curr_date] = datetime.today().strftime(
-            '%Y/%m/%d').split('/')
-        logger.info(f"current time: {curr_year}/{curr_month}/{curr_date}")
-        logger.info(f"end time: {end_yr}/{end_month}/{end_date}")
+            [curr_year, curr_month, curr_date] = datetime.today().strftime(
+                '%Y/%m/%d').split('/')
+            logger.info(f"current time: {curr_year}/{curr_month}/{curr_date}")
+            logger.info(f"end time: {end_yr}/{end_month}/{end_date}")
 
-        if curr_year < end_yr or (curr_year == end_yr and curr_month < end_month) or (curr_year == end_yr and curr_month == end_month and curr_date < end_date):
-            logger.warning('庫藏股回購尚未結束')
-            continue
+            if curr_year < end_yr or (curr_year == end_yr and curr_month < end_month) or (curr_year == end_yr and curr_month == end_month and curr_date < end_date):
+                logger.warning('庫藏股回購尚未結束')
+                continue
 
-        headers = [f"股票{title}" for title in get_stock_info(target, start, "fields")] \
-            + [f"發行量加權股價指數{title}" for title in get_index_info(start, "fields")
-               ][1:]
+            headers = ['股票日期', '股票成交股數', '股票成交金額', '股票開盤價', '股票最高價', '股票最低價', '股票收盤價',
+                       '股票漲跌價差', '股票成交筆數', '大盤成交股數', '大盤成交金額', '大盤成交筆數', '大盤發行量加權股價指數', '大盤漲跌點數']
 
-        data = []
+            data = []
 
-        if start_yr == end_yr and start_month == end_month:
-            stock_entries = get_stock_info(target, start, "data")
-            index_entries = get_index_info(start, "data")
-            rows = []
-            for i, row in enumerate(stock_entries):
-                rows.append(row + index_entries[i][1:])
-            data = data + rows
-        # get months price
-        elif start_yr == end_yr and start_month != end_month:
-            for month in range(int(start_month), int(end_month) + 1):
-                stock_entries = get_stock_info(
-                    target, f"{start_yr}{str(month).rjust(2, '0')}01", "data")
-                index_entries = get_index_info(
-                    f"{start_yr}{str(month).rjust(2, '0')}01", "data")
-                logger.debug(f"stock_entries {len(stock_entries)}")
-
-                logger.debug(f"stock_entries {stock_entries}")
-                logger.debug(f"index_entries {len(index_entries)}")
-
-                logger.debug(f"index_entries {index_entries}")
-
+            if start_yr == end_yr and start_month == end_month:
+                stock_entries = get_stock_info(target, start, "data")
+                index_entries = get_index_info(start, "data")
                 rows = []
                 for i, row in enumerate(stock_entries):
                     rows.append(row + index_entries[i][1:])
                 data = data + rows
-        else:
-            if int(end_yr) - int(start_yr) == 1:
-                for month in range(int(start_month), 13):
+            # get months price
+            elif start_yr == end_yr and start_month != end_month:
+                for month in range(int(start_month), int(end_month) + 1):
                     stock_entries = get_stock_info(
                         target, f"{start_yr}{str(month).rjust(2, '0')}01", "data")
                     index_entries = get_index_info(
                         f"{start_yr}{str(month).rjust(2, '0')}01", "data")
-                    rows = []
-                    for i, row in enumerate(stock_entries):
-                        rows.append(row + index_entries[i][1:])
-                    data = data + rows
+                    logger.debug(f"stock_entries {len(stock_entries)}")
 
-                for month in range(1, int(end_month) + 1):
-                    stock_entries = get_stock_info(
-                        target, f"{end_yr}{str(month).rjust(2, '0')}01", "data")
-                    index_entries = get_index_info(
-                        f"{end_yr}{str(month).rjust(2, '0')}01", "data")
+                    logger.debug(f"stock_entries {stock_entries}")
+                    logger.debug(f"index_entries {len(index_entries)}")
+
+                    logger.debug(f"index_entries {index_entries}")
+
                     rows = []
                     for i, row in enumerate(stock_entries):
                         rows.append(row + index_entries[i][1:])
                     data = data + rows
             else:
-                for month in range(int(start_month), 13):
-                    stock_entries = get_stock_info(
-                        target, f"{start_yr}{str(month).rjust(2, '0')}01", "data")
-                    index_entries = get_index_info(
-                        f"{start_yr}{str(month).rjust(2, '0')}01", "data")
-                    rows = []
-                    for i, row in enumerate(stock_entries):
-                        rows.append(row + index_entries[i][1:])
-                    data = data + rows
-                for year in range(int(start_yr), int(end_yr) + 1):
-                    for month in range(1, 13):
+                if int(end_yr) - int(start_yr) == 1:
+                    for month in range(int(start_month), 13):
                         stock_entries = get_stock_info(
-                            target, f"{year}{str(month).rjust(2, '0')}01", "data")
+                            target, f"{start_yr}{str(month).rjust(2, '0')}01", "data")
                         index_entries = get_index_info(
-                            f"{year}{str(month).rjust(2, '0')}01", "data")
+                            f"{start_yr}{str(month).rjust(2, '0')}01", "data")
                         rows = []
                         for i, row in enumerate(stock_entries):
                             rows.append(row + index_entries[i][1:])
                         data = data + rows
-                for month in range(1, int(end_month) + 1):
-                    stock_entries = get_stock_info(
-                        target, f"{end_yr}{str(month).rjust(2, '0')}01", "data")
-                    index_entries = get_index_info(
-                        f"{end_yr}{str(month).rjust(2, '0')}01", "data")
-                    rows = []
-                    for i, row in enumerate(stock_entries):
-                        rows.append(row + index_entries[i][1:])
-                    data = data + rows
-        # data = np.array(data)
-        logger.debug(f"headers\n{headers}")
-        logger.debug(f"data\n{data}")
 
-        # sanitize the data
-        sanitize_data = []
-        for point in data:
-            logger.debug(f"point {point}")
-            yr, mon, day = point[0].split('/')
-            logger.debug(f"yr {yr}")
-            logger.debug(f"mon {mon}")
-            logger.debug(f"day {day}")
+                    for month in range(1, int(end_month) + 1):
+                        stock_entries = get_stock_info(
+                            target, f"{end_yr}{str(month).rjust(2, '0')}01", "data")
+                        index_entries = get_index_info(
+                            f"{end_yr}{str(month).rjust(2, '0')}01", "data")
+                        rows = []
+                        for i, row in enumerate(stock_entries):
+                            rows.append(row + index_entries[i][1:])
+                        data = data + rows
+                else:
+                    for month in range(int(start_month), 13):
+                        stock_entries = get_stock_info(
+                            target, f"{start_yr}{str(month).rjust(2, '0')}01", "data")
+                        index_entries = get_index_info(
+                            f"{start_yr}{str(month).rjust(2, '0')}01", "data")
+                        rows = []
+                        for i, row in enumerate(stock_entries):
+                            rows.append(row + index_entries[i][1:])
+                        data = data + rows
+                    for year in range(int(start_yr), int(end_yr) + 1):
+                        for month in range(1, 13):
+                            stock_entries = get_stock_info(
+                                target, f"{year}{str(month).rjust(2, '0')}01", "data")
+                            index_entries = get_index_info(
+                                f"{year}{str(month).rjust(2, '0')}01", "data")
+                            rows = []
+                            for i, row in enumerate(stock_entries):
+                                rows.append(row + index_entries[i][1:])
+                            data = data + rows
+                    for month in range(1, int(end_month) + 1):
+                        stock_entries = get_stock_info(
+                            target, f"{end_yr}{str(month).rjust(2, '0')}01", "data")
+                        index_entries = get_index_info(
+                            f"{end_yr}{str(month).rjust(2, '0')}01", "data")
+                        rows = []
+                        for i, row in enumerate(stock_entries):
+                            rows.append(row + index_entries[i][1:])
+                        data = data + rows
+            # data = np.array(data)
+            logger.debug(f"headers\n{headers}")
+            logger.debug(f"data\n{data}")
 
-            if int(yr) + 1911 == int(start_yr) and int(mon) == int(start_month) and int(day) < int(start_date):
-                continue
-            elif int(yr) + 1911 == int(end_yr) and int(mon) == int(end_month) and int(day) > int(end_date):
-                continue
-            else:
-                sanitize_data.append(point)
-        #  write to files
-        recoder = Recorder()
-        recoder.record_to_csv(f"{target}-{start}-{end}",
-                              headers, sanitize_data)
+            # sanitize the data
+            sanitize_data = []
+            for point in data:
+                logger.debug(f"point {point}")
+                yr, mon, day = point[0].split('/')
+                logger.debug(f"yr {yr}")
+                logger.debug(f"mon {mon}")
+                logger.debug(f"day {day}")
+
+                if int(yr) + 1911 == int(start_yr) and int(mon) == int(start_month) and int(day) < int(start_date):
+                    continue
+                elif int(yr) + 1911 == int(end_yr) and int(mon) == int(end_month) and int(day) > int(end_date):
+                    continue
+                else:
+                    sanitize_data.append(point)
+            #  write to files
+            recoder = Recorder()
+            recoder.record_to_csv(f"{target}-{start}-{end}",
+                                  headers, sanitize_data)
 
 
 if __name__ == '__main__':
